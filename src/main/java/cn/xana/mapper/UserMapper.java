382e836212ab25341405fad3005e33f55a8450d4
xana <1186571718@qq.com>
2021-03-27 10:35:34 +0800

    2021/3/27-release

--- src/main/java/cn/xana/mapper/UserMapper.java
+++ src/main/java/cn/xana/mapper/UserMapper.java
@@ -6,7 +6,6 @@ import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 import tk.mybatis.mapper.common.Mapper;
-
 import java.util.List;
 
 @Repository("userMapper")
